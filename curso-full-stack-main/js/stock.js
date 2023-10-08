a6d10cc479e6a52b6c33340c2eb6bfb70613772b
joaPaez <jpaez@escuelasproa.edu.ar>
2023-10-08 15:48:43 -0300

    Initial commit

---
+++ curso-full-stack-main/js/stock.js
@@ -0,0 +1,7 @@
+let stockProductos = [
+    {id: 1, nombre: "Redragon Zeus X", tipo: "auricular", cantidad: 1, precio:  35000, img: 'imagenes/auri.jpg',href:"auri.html"},
+    {id: 2, nombre: "Redragon C502 Capricornus", tipo: "Silla gamer", cantidad: 1, precio: 115000, img: 'imagenes/capt.webp', href:"silla.html"},
+    {id: 3, nombre: "Macik Wand Pro K587-PRO", tipo: "Teclado", cantidad: 1, precio: 37500, img: 'imagenes/D_NQ_NP_629007-MLA46231927922_062021-O.png', href:"teclado.html"},
+    {id: 4, nombre: "Redragon Ruby", tipo: "Monitor", cantidad: 1, precio: 196000, img: 'imagenes/monitor.jpg',href:"monitores.html"},
+    {id: 5, nombre: "Cobra CHROMA M711", tipo: "Mouse", cantidad: 1, precio: 18699, img: 'imagenes/mouse.webp',href:"mouse.html"},
+]
